27e5b61faacf1a76e5be97214d783a95c90c77b1
Frida <fridapalms@live.se>
2026-04-07 12:24:09 +0200

    login.ts

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,8 +1,39 @@
+import axios from "axios";
 import "./style.css";
 
-//Lyssna klick på "Tillbaka till startsidan" leder till /
-document.getElementById("backToStart")?.addEventListener("click", async (e) => {
+//Lyssna efter submit på "logga in"-formuläret
+document.getElementById("loginForm")?.addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  //Hämta användarnamn och lösenord från login-formuläret
+  const userEmail = (document.getElementById("userEmail") as HTMLInputElement).value;
+  const userPassword = (document.getElementById("userPassword") as HTMLInputElement).value;
+
+  //POST-request för inloggning
+  try {
+    const response = await axios.post(
+      "http://localhost:3000/login",
+      {
+        email: userEmail,
+        password: userPassword,
+      },
+      {
+        withCredentials: true,
+      },
+    );
+
+    //Om det gick bra att logga in, statuskod 200-299 = lyckade anrop
+    if (response.status >= 200 && response.status < 300) {
+      sessionStorage.setItem("loggedInUsersName", response.data.username); //Lagra den inloggade användarens namn i sessionStorage
+      location.href = "/"; //Gå tillbaka till startsidan (eller om vi vill till nån annan sida?)
+    }
+  } catch (error) {
+    alert("Inloggningen misslyckades. Försök igen!");
+  }
+});
+
+//Lyssna efter klick på "Tillbaka till startsidan" som leder till startsidan /
+document.getElementById("backToStart")?.addEventListener("click", async (e) => {
+  e.preventDefault();
   location.href = "/";
 });
