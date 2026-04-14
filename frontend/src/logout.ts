dbb382258796af3d5b8f6b7650c37674fc23d9c2
Frida <fridapalms@live.se>
2026-04-14 12:03:55 +0200

    minor changes

--- frontend/src/logout.ts
+++ frontend/src/logout.ts
@@ -1,26 +1,24 @@
 import axios from "axios";
 
 //Vid klick på logga ut-knappen
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("logoutBtn")?.addEventListener("click", async () => {
-    console.log("clicked logout");
-    try {
-      //Skickar req med cookie till backend för att ta bort cookie
-      await axios.post(
-        "http://localhost:3000/logout",
-        {},
-        {
-          withCredentials: true,
-        },
-      );
+document.getElementById("logoutBtn")?.addEventListener("click", async () => {
+  console.log("clicked logout");
+  try {
+    //Skickar req till backend för att ta bort cookie
+    await axios.post(
+      "http://localhost:3000/logout",
+      {},
+      {
+        withCredentials: true,
+      },
+    );
 
-      //Ta bort inloggade användaren från sessionStorage
-      sessionStorage.removeItem("loggedInUsersName");
+    //Ta bort inloggade användaren från sessionStorage
+    sessionStorage.removeItem("loggedInUsersName");
 
-      //Uppdatera sidan
-      location.href = "/";
-    } catch (error) {
-      console.error(error);
-    }
-  });
+    //Uppdatera sidan
+    location.href = "/";
+  } catch (error) {
+    console.error("Logout failed: ", error);
+  }
 });
