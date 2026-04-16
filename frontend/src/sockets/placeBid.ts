d95d8e1ada89b9d681bfd30c668a07143be41fe1
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2026-04-16 13:42:35 +0200

    update

--- frontend/src/sockets/placeBid.ts
+++ frontend/src/sockets/placeBid.ts
@@ -2,7 +2,6 @@ import type { Socket } from "socket.io-client";
 import type { Bid } from "../models/Bid.ts";
 import { createHtmlForBid } from "../utils/bidUtil.ts";
 import type { AuctionState } from "../models/AuctionState.ts";
-<<<<<<< HEAD
 
 // export function initPlaceBid(socket: Socket) {
 //   socket.on("newBid", (data: any) => {
@@ -18,9 +17,6 @@ import type { AuctionState } from "../models/AuctionState.ts";
 // export function placeBid(auctionId: string, bidAmount: number, userId: string) {
 //   socket.emit("placeBid", { auctionId, bidAmount, userId });
 // }
-=======
-import { updateDetails } from "../utils/updateDetailsUtil.ts";
->>>>>>> 94fbc744332a28fc378257190a8b66e7e33a999b
 
 // export const initPlaceBid = (socket: Socket, state: AuctionState) => {
  // socket.off("newBid");
@@ -34,7 +30,6 @@ import { updateDetails } from "../utils/updateDetailsUtil.ts";
 //  });
 // };
 
-<<<<<<< HEAD
 const handleNewBid = (state: AuctionState) => (newBid: Bid) => {
   console.log(`Nytt bud: ${newBid.bid}kr`);
 
@@ -42,16 +37,7 @@ const handleNewBid = (state: AuctionState) => (newBid: Bid) => {
   if (!auctionId) return;
 
   createHtmlForBid(newBid, auctionId);
-=======
-    createHtmlForBid(newBid, auctionId);
-    updateDetails(auctionId, newBid); //
-  });
-
-  socket.on("bidError", (message) => {
-    console.log(message);
-    alert(message);
-  });
->>>>>>> 94fbc744332a28fc378257190a8b66e7e33a999b
+  updateDetails(auctionId, newBid);
 };
 
 const handleBidError = (message: string) => {
@@ -60,9 +46,9 @@ const handleBidError = (message: string) => {
 };
 
 export const initPlaceBid = (socket: Socket, state: AuctionState) => {
-socket.off("newBid");
-socket.off("bidError");
+  socket.off("newBid");
+  socket.off("bidError");
 
-socket.on("newBid", handleNewBid(state));
-socket.on("bidError", handleBidError);
+  socket.on("newBid", handleNewBid(state));
+  socket.on("bidError", handleBidError);
 };
