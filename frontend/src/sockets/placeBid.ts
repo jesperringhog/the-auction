43cbdba6d5c4450f5e0b2a80d1199823058976df
Frida <fridapalms@live.se>
2026-04-16 15:55:35 +0200

    fixed import

--- frontend/src/sockets/placeBid.ts
+++ frontend/src/sockets/placeBid.ts
@@ -2,6 +2,7 @@ import type { Socket } from "socket.io-client";
 import type { Bid } from "../models/Bid.ts";
 import { createHtmlForBid } from "../utils/bidUtil.ts";
 import type { AuctionState } from "../models/AuctionState.ts";
+import { updateDetails } from "../utils/updateDetailsUtil.ts";
 
 // export function initPlaceBid(socket: Socket) {
 //   socket.on("newBid", (data: any) => {
@@ -19,10 +20,10 @@ import type { AuctionState } from "../models/AuctionState.ts";
 // }
 
 // export const initPlaceBid = (socket: Socket, state: AuctionState) => {
- // socket.off("newBid");
- // socket.off("bidError");
+// socket.off("newBid");
+// socket.off("bidError");
 
- // socket.on("newBid", (newBid: Bid) => {
+// socket.on("newBid", (newBid: Bid) => {
 //  console.log(`Nytt bud: ${newBid.bid}kr`);
 //    const auctionId = state.selectedAuction;
 //    if (!auctionId) return;
