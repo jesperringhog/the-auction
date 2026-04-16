94fbc744332a28fc378257190a8b66e7e33a999b
Frida <fridapalms@live.se>
2026-04-16 12:59:49 +0200

    added alternating colors for bid history

--- frontend/src/utils/bidUtil.ts
+++ frontend/src/utils/bidUtil.ts
@@ -3,17 +3,26 @@ import type { Bid } from "../models/Bid";
 export const createHtmlForBid = (bid: Bid, auctionId: string | null) => {
   const bidContainer = document.getElementById(`bidContainer-${auctionId}`);
 
+  const bidRow = document.createElement("div");
   const userTag = document.createElement("p");
   const bidTag = document.createElement("p");
   const timeTag = document.createElement("p");
 
   const time = new Date(bid.time);
+  const loggedIn = sessionStorage.getItem("loggedInUsersName");
 
+  bidRow.className = "bidRow";
   userTag.textContent = `Budgivare: ${bid.user.username}`;
   bidTag.textContent = `Bud: ${bid.bid.toString()}kr`;
   timeTag.textContent = `${time.toLocaleDateString()}${time.toLocaleTimeString()}`;
 
-  bidContainer?.appendChild(userTag);
-  bidContainer?.appendChild(bidTag);
-  bidContainer?.appendChild(timeTag);
+  if (loggedIn === bid.user.username) {
+    userTag.textContent = `Budgivare: ${bid.user.username} (Ditt bud)`;
+    userTag.className = "bold-text";
+  }
+
+  bidRow.appendChild(userTag);
+  bidRow.appendChild(bidTag);
+  bidRow.appendChild(timeTag);
+  bidContainer?.appendChild(bidRow);
 };
